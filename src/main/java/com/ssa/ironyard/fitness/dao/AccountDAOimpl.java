dd708c58a5b0bc78a3e3e7b7f6b553d13c3a9c7c
David Shea <dsheashea@gmail.com>
2016-10-03 15:14:13 -0400

    git ignore updated

---
+++ src/main/java/com/ssa/ironyard/fitness/dao/AccountDAOimpl.java
@@ -0,0 +1,53 @@
+package com.ssa.ironyard.fitness.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+
+import com.ssa.ironyard.fitness.model.Account;
+
+public class AccountDAOimpl extends AbstractSpringDAO<Account> {
+
+    final ORM<Account> orm;
+    final DataSource dataSource;
+
+    protected AccountDAOimpl(ORM<Account> orm, DataSource dataSource) {
+        super(orm, dataSource);
+    }
+    
+    @Autowired
+    public AccountDAOimpl(DataSource datasource) {
+    this(datasource, new AccountORM() {
+    });
+    }
+    
+
+    @Override
+    protected void insertPreparer(PreparedStatement insertStatement, Account domainToInsert) throws SQLException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected Account afterInsert(Account copy, Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Account afterUpdate(Account copy) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected PreparedStatementSetter updatePreparer(Account domainToUpdate) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/main/java/com/ssa/ironyard/fitness/dao/AccountORM.java
@@ -0,0 +1,34 @@
+package com.ssa.ironyard.fitness.dao;
+
+import java.sql.ResultSet;
+
+import com.ssa.ironyard.fitness.model.Account;
+
+public interface AccountORM extends ORM<Account> {
+
+    default String projection() {
+        return table() + ".id, username, password, first_name, last_name, height, weight, sex, age, goal_id, history_id";
+    };
+
+    String eagerProjection();
+
+    default String table() {
+        return "accounts";
+    }
+
+    Account map(ResultSet results);
+
+    String prepareInsert();
+
+    String prepareUpdate();
+
+    String prepareRead();
+
+    String prepareDelete();
+
+    String prepareSearch();
+
+    String prepareEagerRead();
+
+    Account eagerMap(ResultSet results);
+}
