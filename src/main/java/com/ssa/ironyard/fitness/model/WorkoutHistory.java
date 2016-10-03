8cc40699a3a82456da6efc2261743ac7fbb0d4aa
David Shea <dsheashea@gmail.com>
2016-10-03 14:20:33 -0400

    Domain Objects finished

---
+++ src/main/java/com/ssa/ironyard/fitness/model/WorkoutHistory.java
@@ -0,0 +1,132 @@
+package com.ssa.ironyard.fitness.model;
+
+import java.util.Calendar;
+
+public class WorkoutHistory implements DomainObject{ 
+Integer id;
+
+public Integer getId() {
+    return id;
+}
+
+public void setId(Integer id) {
+    this.id = id;
+}
+
+public Calendar getWorkout_date() {
+    return workout_date;
+}
+
+public void setWorkout_date(Calendar workout_date) {
+    this.workout_date = workout_date;
+}
+
+public Exercise getExercise() {
+    return exercise;
+}
+
+public void setExercise(Exercise exercise) {
+    this.exercise = exercise;
+}
+
+public int getReps() {
+    return reps;
+}
+
+public void setReps(int reps) {
+    this.reps = reps;
+}
+
+public float getWeight() {
+    return weight;
+}
+
+public void setWeight(float weight) {
+    this.weight = weight;
+}
+
+Calendar workout_date;
+Exercise exercise;
+int reps;
+float weight;
+
+public WorkoutHistory() {
+    // TODO Auto-generated constructor stub
+}
+
+@Override
+public String toString() {
+    return "WorkoutHistory [id=" + id + ", workout_date=" + workout_date + ", exercise=" + exercise + ", reps=" + reps
+            + ", weight=" + weight + "]";
+}
+
+@Override
+public WorkoutHistory clone() {
+
+    try{
+        WorkoutHistory wh = (WorkoutHistory) super.clone();
+        wh.setExercise(this.exercise.clone());
+        return wh;
+    }
+    catch(Exception e){
+        
+    }
+    return null;
+}
+
+@Override
+public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((exercise == null) ? 0 : exercise.hashCode());
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + reps;
+    result = prime * result + Float.floatToIntBits(weight);
+    result = prime * result + ((workout_date == null) ? 0 : workout_date.hashCode());
+    return result;
+}
+
+@Override
+public boolean equals(Object obj) {
+    WorkoutHistory other = (WorkoutHistory) obj;
+    if (id == null) {
+        if (other.id != null)
+            return false;
+    } else if (!id.equals(other.id))
+        return false;
+    return true;
+}
+
+@Override
+public boolean deeplyEquals(DomainObject obj) {
+    if (this == obj)
+        return true;
+    if (obj == null)
+        return false;
+    if (getClass() != obj.getClass())
+        return false;
+    WorkoutHistory other = (WorkoutHistory) obj;
+    if (exercise == null) {
+        if (other.exercise != null)
+            return false;
+    } else if (!exercise.equals(other.exercise))
+        return false;
+    if (id == null) {
+        if (other.id != null)
+            return false;
+    } else if (!id.equals(other.id))
+        return false;
+    if (reps != other.reps)
+        return false;
+    if (Float.floatToIntBits(weight) != Float.floatToIntBits(other.weight))
+        return false;
+    if (workout_date == null) {
+        if (other.workout_date != null)
+            return false;
+    } else if (!workout_date.equals(other.workout_date))
+        return false;
+    return true;
+}
+
+
+}
