6ae4c27df01a9ed2f9a525856e9d7d7fe81ada5e
Joe Brown <jbrown762@gmail.com>
2016-10-04 13:00:43 -0400

    exercise controller

--- src/main/java/com/ssa/ironyard/fitness/controller/FitnessExerciseController.java
+++ src/main/java/com/ssa/ironyard/fitness/controller/FitnessExerciseController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssa.ironyard.fitness.model.Account;
 import com.ssa.ironyard.fitness.model.Exercise;
 import com.ssa.ironyard.fitness.services.FitnessAccountServiceImpl;
+import com.ssa.ironyard.fitness.services.FitnessExerciseServiceImpl;
 
 @RestController
 @RequestMapping(value = "/fitness")
@@ -32,66 +33,30 @@ public class FitnessExerciseController
     private FitnessExerciseServiceImpl service;
 
     @RequestMapping(produces = "application/json", value = "/exercises", method = RequestMethod.GET)
-    public ResponseEntity<Map<String, Exercise>> getExerciseList(@PathVariable String username)
+    public ResponseEntity<List<Exercise>> getExerciseList(@PathVariable String username)
     {
         ResponseEntity.status(HttpStatus.CREATED);
-        HashMap<String ,List<Exercise>> map = new HashMap<String, List<Exercise>>();
+        HashMap<String, List<Exercise>> map = new HashMap<String, List<Exercise>>();
         List<Exercise> list = service.readAllExercises();
-        
-        if(list.size() == 0)
-        {
+
+        if (list.size() == 0)
             map.put("error", list);
-            return ResponseEntity.ok().header("SSA_Bank Customer", "Account").body(map);
-        }
         else
-        {
             map.put("success", list);
-            return ResponseEntity.ok().header("SSA_Bank Customer", "Account").body(map);
-        }
         
-        
+        return ResponseEntity.ok().header("SSA_Bank Customer", "Account").body(list);
+
     }
 
-    @RequestMapping(produces = "application/json", value = "/{username}/{password}", method = RequestMethod.POST)
-    public ResponseEntity<Map<String, Account>> createAccount(@PathVariable String username,
-            @PathVariable String password)
-    {
-        ResponseEntity.status(HttpStatus.CREATED);
-        Map<String, Account> map = new HashMap<>();
+  
+ 
 
-        Account a = service.insertAccount(username, password);
-
-        if (a == null)
-            map.put("error", a);
-        else
-            map.put("success", a);
-
-        return ResponseEntity.ok().header("Fitness Account", "Account").body(map);
-    }
-
-    @RequestMapping(produces = "application/json", value = "/{username}/{password}", method = RequestMethod.PUT)
-    public ResponseEntity<Map<String, Account>> updateAccount(@PathVariable String username,
-            @PathVariable String password, HttpServletRequest request)
-    {
-        ResponseEntity.status(HttpStatus.CREATED);
-        Map<String, Account> map = new HashMap<>();
-
-        Account a = service.updateAccount(username, password, request);
-
-        if (a == null)
-            map.put("error", a);
-        else
-            map.put("success", a);
-
-        return ResponseEntity.ok().header("Fitness Account", "Account").body(map);
-    }
-
-//    @RequestMapping(produces = "application/json", value = "/{username}/{password}", method = RequestMethod.DELETE)
-//    public ResponseEntity<Map<String, Account>> deleteAccount(@PathVariable String username,
-//            @PathVariable String password)
-//    {
-//        if (service.readAccount(username) != null)
-//            return service.deleteAccount(username, password);
-//
-//    }
+    // @RequestMapping(produces = "application/json", value = "/{username}/{password}", method = RequestMethod.DELETE)
+    // public ResponseEntity<Map<String, Account>> deleteAccount(@PathVariable String username,
+    // @PathVariable String password)
+    // {
+    // if (service.readAccount(username) != null)
+    // return service.deleteAccount(username, password);
+    //
+    // }
 }
---
+++ src/main/java/com/ssa/ironyard/fitness/services/FitnessExerciseServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ssa.ironyard.fitness.services;
+
+import java.util.List;
+
+import com.ssa.ironyard.fitness.model.Exercise;
+
+public class FitnessExerciseServiceImpl
+{
+
+    public List<Exercise> readAllExercises()
+    {
+        
+        return null;
+    }
+
+}
