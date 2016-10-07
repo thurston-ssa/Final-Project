86294d4d410c16303709e3f0189692d50e325cfa
David Shea <dsheashea@gmail.com>
2016-10-06 20:59:51 -0400

    messing with angular

--- src/main/resources/static/js/controllers/loginController.js
+++ src/main/resources/static/js/controllers/loginController.js
@@ -1,32 +1,36 @@
 console.log("hello");
-angular 
-.module("Fitness") 
-.controller("loginController", login) 
+angular
+    .module("Fitness")
+    .controller("loginController", login)
 
-login.$inject =['$state', '$http'] 
+login.$inject = ['$state', '$http']
 
-function login($state, $http) { 
-	var ctrl = this;
-	return {
-		submitter:function(){
-			 ctrl = this;
-			
-			$http.post("http://localhost:8080/fitness/" + ctrl.username +"/"+ ctrl.password).then(function(res) {
-				if(res.data.success){
-					console.log("success");
-					console.log(res.data);
-					console.log(ctrl.flag = true);
-					$state.go("AccountPage", {username: ctrl.username})
-			
-				}
-				else{
-					console.log("youre stupid");
-					$state.go("login");
-					//console.log("didn't work!");
-				}
-			})
-		}
-	}
+function login($state, $http) {
+    var ctrl = this;
+    ctrl.flag;
+
+    return {
+        submitter: function () {
+            ctrl = this;
+            ctrl.flag = false;
+            $http.post("http://localhost:8080/fitness/" + ctrl.username + "/" + ctrl.password).then(function (res) {
+                if (res.data.success) {
+                    console.log("success");
+                    console.log(res.data);
+                    console.log(ctrl.flag);
+                    ctrl.flag = true;
+                    $state.go("AccountPage", {
+                        username: ctrl.username
+                    })
+
+                } else {
+                    console.log("youre stupid");
+                    $state.go("login");
+                    //console.log("didn't work!");
+                }
+            })
+        }
+    }
 }
 
 //var ctrl = this
--- src/main/resources/static/js/services/accounts.js
+++ src/main/resources/static/js/services/accounts.js
@@ -1,16 +1,17 @@
-angular 
-.module("Fitness") 
-.factory("Accounts", Accounts) 
+angular
+    .module("Fitness")
+    .factory("Accounts", Accounts)
 
 //FOR ADMIN
 
-Accounts.$inject =['$http'] 
-function Accounts($http) { 
-	 return {
-		all: function(){
-			return $http.get("http://localhost:8080/fitness/accounts").then(function(res) {
-			return res.data })
-		} 	 
-	 }
+Accounts.$inject = ['$http']
+
+function Accounts($http) {
+    return {
+        all: function () {
+            return $http.get("http://localhost:8080/fitness/accounts").then(function (res) {
+                return res.data
+            })
+        }
+    }
 }
- 
