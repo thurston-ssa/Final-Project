a56c26564aeead774788c127ab6a963691b2e94e
Joe Brown <jbrown762@gmail.com>
2016-10-12 15:42:28 -0400

    cleaned up merge conflicts

--- src/main/resources/static/js/services/exercises.js
+++ src/main/resources/static/js/services/exercises.js
@@ -1,29 +1,19 @@
 console.log("youre in exercise factory");
 
-angular
-    .module("Fitness")
-    .factory("Exercises", Exercises)
+angular.module("Fitness").factory("Exercises", Exercises)
 
-Exercises.$inject = ['$http']
+Exercises.$inject = [ '$http' ]
 
 function Exercises($http) {
-    console.log("exercises function");
-    return {
-        all: function () {
-            console.log("before the get request");
-            return $http.get("http://localhost:8080/fitness/home/AllExercises").then(function (response) {
-<<<<<<< HEAD
+	console.log("exercises function");
+	return {
+		all : function() {
+			console.log("before the get request");
+			return $http.get("http://localhost:8080/fitness/home/AllExercises").then(function(response) {
 
+				return response.data;
+			})
+		}
+	}
 
-                    return response.data;
-                }
-            }
-        }
-=======
-                return response.data;
-
-            })
-        }
-    }
 }
->>>>>>> 127d8dff5b1183662603f4683c45c5eb79617ea8
