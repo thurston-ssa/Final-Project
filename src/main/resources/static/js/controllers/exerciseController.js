a749f1e85235597dee0f545bb67c956aa66c14f2
Ami Shah <s90amii@gmail.com>
2016-10-12 14:18:57 -0400

    ...

--- src/main/resources/static/js/controllers/exerciseController.js
+++ src/main/resources/static/js/controllers/exerciseController.js
@@ -8,14 +8,13 @@ exercise.$inject = ['Exercises']
 function exercise(Exercises) {
     console.log("here");
     var ctrl = this;
-
+    ctrl.arms = [];
+    ctrl.legs = [];
     Exercises.all().then(function (exercises) {
-<<<<<<< HEAD
-        return ctrl.list = exercises;
-=======
+
         console.log(exercises);
         ctrl.list = exercises;
->>>>>>> 4a324b6eff8709e50c42e79d8aafa39a2bc2416c
+        
     })
-
+    
 }
--- src/main/resources/static/js/services/exercises.js
+++ src/main/resources/static/js/services/exercises.js
@@ -12,20 +12,8 @@ function Exercises($http) {
         all: function () {
             console.log("before the get request");
             return $http.get("http://localhost:8080/fitness/home/AllExercises").then(function (response) {
-<<<<<<< HEAD
-                return response.data.success;
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-                console.log(response.data.success);
-                return response.data.success;
-=======
-
->>>>>>> e8282a88f96b845d0027b444b2455347845d6b2b
                 return response.data;
->>>>>>> d73aadca21407cb272bfddd2c0f385f83263117c
->>>>>>> 4a324b6eff8709e50c42e79d8aafa39a2bc2416c
+
             })
         }
     }
