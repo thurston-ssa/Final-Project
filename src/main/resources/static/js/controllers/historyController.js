499128cce074c96ddadc1c88f7cef7c745bd69e0
David Shea <dsheashea@gmail.com>
2016-10-13 14:29:34 -0400

    updating workout history form

--- src/main/resources/static/js/controllers/historyController.js
+++ src/main/resources/static/js/controllers/historyController.js
@@ -7,7 +7,7 @@ history.$inject = ['$state', '$http', '$location']
 function history($state, $http, $location) {
     var ctrl = this;
     var path = $location.absUrl();
-    var url = path.substring(0, ($location.path.length) + 1);
+    var url = path.substring(0, 37);
     console.log(url);
     return $http.get(url + "/history").then(function (response) {
         console.log(response.data);
--- src/main/resources/static/js/controllers/workoutHistory.js
+++ src/main/resources/static/js/controllers/workoutHistory.js
@@ -1,11 +1,38 @@
 angular.module("Fitness").controller("workoutHistoryController", history)
 
-history.$inject = ['$http', '$state']
+history.$inject = ['$http', '$state', '$location']
 
-function history($http, $state) {
+function history($http, $state, $location) {
     var ctrl = this;
+    var path = $location.absUrl();
+    var length = ($location.absUrl().length) - ($location.path().length)
+    ctrl.list = [];
+    console.log(length)
+    var url = path.substring(35, length - 1) + "/history";
 
-    console.log('State is', $state)
-    console.log('id is', $state.params.id)
-        
+    var data = $.param({
+        id: url,
+        workout_date: ctrl.date,
+        weight: ctrl.weight,
+        height: ctrl.height,
+        reps: ctrl.reps,
+        sets: ctrl.sets,
+        distance: ctrl.distance,
+        duration: ctrl.time
+    });
+
+    var config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+        }
+    }
+
+
+    ctrl.submitForm = function () {
+        ctrl.list.push(data);
+        $http.post(url, data, config).then(function (response) {
+            console.log(response);
+        });
+
+    }
 }
