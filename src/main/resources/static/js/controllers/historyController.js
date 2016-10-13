3a176c6f032d2f395aef206c438005497fa337c3
David Shea <dsheashea@gmail.com>
2016-10-13 13:21:54 -0400

    updated index links:

--- src/main/resources/static/js/controllers/historyController.js
+++ src/main/resources/static/js/controllers/historyController.js
@@ -7,7 +7,7 @@ history.$inject = ['$state', '$http', '$location']
 function history($state, $http, $location) {
     var ctrl = this;
     var path = $location.absUrl();
-    var url = path.substring(0, path.length - 9);
+    var url = path.substring(0, ($location.path.length) + 1);
     console.log(url);
     return $http.get(url + "/history").then(function (response) {
         console.log(response.data);
