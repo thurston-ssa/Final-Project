9ddecc8226a52a2f444a84b3c442f98bd5c205c8
Joe Brown <jbrown762@gmail.com>
2016-10-17 16:35:03 -0400

    regimen

--- src/main/resources/static/js/controllers/auController.js
+++ src/main/resources/static/js/controllers/auController.js
@@ -1,22 +1,31 @@
 console.log("found account controller")
-angular
-    .module("Fitness")
-    .controller("AccountController", accountpage)
+angular.module("Fitness").controller("AccountController", accountpage)
 
-accountpage.$inject = ['$state', '$http', '$location']
+accountpage.$inject = [ '$state', '$http', '$location', '$scope' ]
 
-function accountpage($state, $http, $location) {
-	var ctrl = this;
-    var path = $location.absUrl();
-    var length = ($location.absUrl().length) - ($location.path().length);
-    console.log(length);
-    var url = path.substring(35, length-1);
-    console.log(url);
+function accountpage($state, $http, $location, $scope) {
+	var path = $location.absUrl();
+	var length = ($location.absUrl().length) - ($location.path().length);
+	console.log(length);
+	var url = path.substring(35, length - 1);
+	console.log(url);
 
-   return  $http.get("http://localhost:8080/fitness/home/" + url ).then(function(res) {
-    		console.log( res.data.success.username);
-    		ctrl.username = res.data.success.username;
-    		
-	    })
-    
+	$scope.regimen = [  ]
+
+	return $http.get("http://localhost:8080/fitness/home/" + url).then(function(res) {
+		console.log("http://localhost:8080/fitness/home/" + url)
+		console.log(res.data);
+		$scope.username = res.data.success.username;
+		
+		return $http.get("http://localhost:8080/fitness/home/" + url + "/regimen").then(function(res) {
+			console.log("http://localhost:8080/fitness/home/" + url + "/regimen")
+			console.log(res.data.success);
+			
+			 for (i = 0; i < res.data.success.length; i++) {
+				 $scope.regimen.push(res.data.success[i])
+			 }
+			
+		})
+	})
+
 }
