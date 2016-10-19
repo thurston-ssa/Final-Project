dcec318306c6e4615a3feb1e516fb79d049f431e
thurston <thurston@theironyard.com>
2016-10-19 09:26:33 -0400

    Better tests and had to add a getExercises method so jackson will marshal the list of exercises in a WorkoutRegimen

--- src/test/java/com/ssa/ironyard/fitness/model/WorkoutRegimenTests.java
+++ src/test/java/com/ssa/ironyard/fitness/model/WorkoutRegimenTests.java
@@ -1,6 +1,9 @@
 package com.ssa.ironyard.fitness.model;
 
 import com.ssa.ironyard.fitness.model.Regimen.DAY;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.Test;
 /**
@@ -39,5 +42,81 @@ public class WorkoutRegimenTests
         workoutRegimen.add(exercise);
         
         assertEquals("", user, workoutRegimen.getAccount());
+    }
+    
+    @Test
+    public void iterationOrder()
+    {
+        WorkoutRegimen workoutRegimen = new WorkoutRegimen();
+        Account user = new Account(Integer.SIZE);
+        
+        Regimen sundayBench = new Regimen();
+        sundayBench.setAccount(user);
+        sundayBench.setExercise(new Exercise((int) 'B'));
+        sundayBench.setDay(DAY.Sunday);
+        workoutRegimen.add(sundayBench);
+        
+        Regimen wednesdaySwim = new Regimen();
+        wednesdaySwim.setAccount(user);
+        wednesdaySwim.setDay(DAY.Wednesday);
+        wednesdaySwim.setExercise(new Exercise((int) 'S'));
+        workoutRegimen.add(wednesdaySwim);
+        
+        Regimen mondayCurl = new Regimen();
+        mondayCurl.setAccount(user);
+        mondayCurl.setDay(DAY.Monday);
+        mondayCurl.setExercise(new Exercise((int) 'C'));
+        workoutRegimen.add(wednesdaySwim);
+        
+        //OK, iteration order should be by day-of-week
+        List<Regimen> expected = new ArrayList<>(Arrays.asList(sundayBench, mondayCurl, wednesdaySwim));
+        List<Regimen> actual = new ArrayList<>();
+        for (Regimen regimen : workoutRegimen)
+        {
+            actual.add(regimen);
+        }
+        assertEquals("", expected, actual);
+    }
+    
+    @Test
+    public void dayFilter()
+    {
+        WorkoutRegimen regimen = new WorkoutRegimen();
+        Account account = new Account(Integer.SIZE);
+        
+        Regimen monday = new Regimen();
+        monday.setDay(DAY.Monday);
+        monday.setAccount(account);
+        monday.setExercise(new Exercise((int) 'J'));
+        regimen.add(monday);
+        
+        Regimen wednesday = new Regimen();
+        wednesday.setAccount(account);
+        wednesday.setExercise(new Exercise((int) 'S'));
+        wednesday.setDay(DAY.Wednesday);
+        regimen.add(wednesday);
+        
+        monday = new Regimen();
+        monday.setDay(DAY.Monday);
+        monday.setAccount(account);
+        monday.setExercise(new Exercise((int) 'S'));
+        regimen.add(monday);
+        
+        
+        List<Regimen> mondays = regimen.daily(DAY.Monday);
+        assertEquals("", 2, mondays.size());
+        for (Regimen mon : mondays)
+        {
+            assertTrue("", mon.getDay() == DAY.Monday);
+        }
+        assertEquals("", 1, mondays.stream().filter(ex -> ex.exercise.equals(new Exercise((int) 'J'))).count());
+        assertEquals("", 1, mondays.stream().filter(ex -> ex.exercise.equals(new Exercise((int) 'S'))).count());
+        
+        List<Regimen> wednesdays = regimen.daily(DAY.Wednesday);
+        assertEquals("", wednesdays, Arrays.asList(wednesday));
+        
+        assertEquals("", Arrays.asList(DAY.Monday, DAY.Wednesday), regimen.activeDays());
+        assertEquals("", Arrays.asList(DAY.Sunday, DAY.Tuesday, DAY.Thursday, DAY.Friday, DAY.Saturday), 
+                         regimen.restDays());
     }
 }
